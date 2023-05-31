b1aa5454ae47d628f6ded9a0c07069b015116180
rayyaalika <rayyaalika@gmail.com>
2023-05-31 15:12:06 +0700

    b

--- assets/js/langgam_beban.js
+++ assets/js/langgam_beban.js
@@ -1,67 +1,173 @@
-// const xValues = [100,200,300,400,500,600,700,800,900,1000];
+// Mendapatkan elemen canvas
+// var canvas = document.getElementById("lineChart");
+// var ctx = canvas.getContext("2d");
+
+// // Data untuk grafik garis
+// var data = {
+//   labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+//   datasets: [
+//     {
+//       data: [10, 20, 15, 25, 30, 20],
+//       borderColor: "#5041BC",
+//       borderWidth: 2 // Lebar garis
+//     }
+//   ]
+// };
+
+// // Membuat grafik garis menggunakan Chart.js
+// var lineChart = new Chart(ctx, {
+//   type: "line",
+//   data: data,
+//   options: {}
+// });
+// Ambil elemen canvas
+
+const chartData = {
+  labels: [
+    "00:00",
+    "00:30",
+    "01:00",
+    "01:30",
+    "02:00",
+    "02:30",
+    "03:00",
+    "03:30",
+    "04:00",
+    "04:30",
+    "05:00",
+    "05:30",
+    "06:00",
+    "06:30",
+    "07:00",
+    "07:30",
+    "08:00",
+    "08:30",
+    "09:00",
+    "09:30",
+    "10:00",
+    "10:30",
+    "11:00",
+    "11:30",
+    "12:00",
+    "12:30",
+    "13:00",
+    "13:30",
+    "14:00",
+    "14:30",
+    "15:00",
+    "15:30",
+    "16:00",
+    "16:30",
+    "17:00",
+    "17:30",
+    "18:00",
+    "18:30",
+    "19:00",
+    "19:30",
+    "20:00",
+    "20:30",
+    "21:00",
+    "21:30",
+    "22:00",
+    "22:30",
+    "23:00",
+    "23:30",
+  ],
+  datasets: [
+    {
+      label: "Data Aktual",
+      data: [69.43, 68.49 , 68.29 , 67.40 , 67.17 , 66.18 , 65.51 , 65.96 , 65.57 , 64.93 , 64.87 , 63.23 , 60.69 , 59.98 , 59.52 ,
+        59.61 , 57.52 , 55.49 , 55.43 , 55.58 , 55.90 , 55.62 , 55.68 , 55.39 , 55.16 , 55.72 , 56.42 , 55.87 , 55.60 , 55.51 , 55.39 , 54.16 ,
+        54.85 , 55.25 , 57.03 , 60.61 , 68.81 , 81.01 , 85.76 , 84.47 , 83.23 , 82.42 , 81.08 , 79.41 , 77.64 , 75.76 , 73.64 , 73.09 ],
+      borderColor: "red",
+      backgroundColor: "rgba(255, 0, 0, 0.2)",
+      borderWidth: 2, 
+    },
+    {
+      label: "Data Prediksi",
+      data: [5, 15, 10, 20, 25],
+      borderColor: "blue",
+      backgroundColor: "rgba(0, 0, 255, 0.2)",
+      borderWidth: 2
+    }
+    // {
+    //   backgroundColor: hexToRgba("#ff0000", 10),
+    //   borderColor: "#ff0000",
+    //   pointHoverBackgroundColor: "#ff0000",
+    //   borderWidth: 2,
+    //   data: realtimes_langgam,
+    //   fill: true,
+    // },
+    // {
+    //   backgroundColor: hexToRgba("#0000FF", 10),
+    //   borderColor: "#0000FF",
+    //   pointHoverBackgroundColor: "#0000FF",
+    //   borderWidth: 2,
+    //   data: realtimes_langgam_prediksi,
+    //   fill: true,
+    // },
+  ]
+};
+
+const ctx = document.getElementById("chart").getContext("2d");
+new Chart(ctx, {
+  type: "line",
+  data: chartData,
+  options: {
+    scales: {
+      x: {
+        grid: {
+          drawOnChartArea: false,
+        },
+      },
+      y: {
+        ticks: {
+          beginAtZero: true,
+          maxTicksLimit: 10,
+          stepSize: Math.ceil(10 / 5),
+          max: 5,
+        },
+      },
+    },
+    plugins: {
+      title: {
+        display: false
+      }
+    },
+    elements: {
+      line: {
+        tension: 0.4,
+      },
+      point: {
+        radius: 0,
+        hitRadius: 10,
+        hoverRadius: 4,
+        hoverBorderWidth: 3,
+      },
+    },
+  }
+});
+
+
+// const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+// const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
 // new Chart("myChart", {
 //   type: "line",
 //   data: {
 //     labels: xValues,
-//     datasets: [{ 
-//       data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-//       borderColor: "red",
-//       fill: false
-//     }, { 
-//       data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-//       borderColor: "green",
-//       fill: false
-//     }, { 
-//       data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-//       borderColor: "blue",
-//       fill: false
+//     datasets: [{
+//       fill: false,
+//       lineTension: 0,
+//       backgroundColor: "rgba(0,0,255,1.0)",
+//       borderColor: "rgba(0,0,255,0.1)",
+//       data: yValues
 //     }]
 //   },
 //   options: {
-//     legend: {display: false}
+//     legend: {display: false},
+//     scales: {
+//       yAxes: [{ticks: {min: 6, max:16}}],
+//     }
 //   }
 // });
-// const ctx = document.getElementById('myChart');
-
-//   new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true
-//         }
-//       }
-//     }
-//   });
-
-const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-const yValues = [7,8,8,9,9,9,10,11,14,14,15];
-
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    scales: {
-      yAxes: [{ticks: {min: 6, max:16}}],
-    }
-  }
-});
--- realtime.php
+++ realtime.php
@@ -1,70 +1,38 @@
 <div class="header">
-     <a>Realtime</a>
+    <a>Realtime</a>
 </div>
 <!-- <div class="main-relatime">
-            <div class="cards-4">
-                <div class="card">
-                    <div class="card-name">Beban Pembangkit</div>
-                </div>
-                <div class="card">
-                    <div class="card-name">Frequency</div>
-                </div>
-                <div class="card">
-                    <div class="card-name">Losses</div>
-                </div>
-                <div class="card">
-                    <div class="card-name">Fuelmix</div>
-                </div>
-                <div class="card">
-                    <div class="card-name">Biaya Energi</div>
-                </div>
+    <div class="cards-1">
+        <div class="card">
+            <div class="card-name">Langgam Beban Timor</div>
+            <div class="box-chart">
+                  <canvas id="myChart" style="width:100%;max-height:400px"></canvas>
             </div>
+            </div>
+        </div>
+    </div>
 </div> -->
-<div class="main-relatime">
-            <div class="cards-1">
-                <div class="card">
-                <div class="card-name">Langgam Beban Timor</div>
-                <div class="box-chart">
-                <canvas id="myChart" style="width:100%;max-height:400px"></canvas>
-                </div>
-                </div>
+
+<div class="main-realtime">
+    <div class="main-right">
+        <div class="card">
+            <div class="card-name-right">Langgam Beban Timor</div>
+            <div class="box-chart">
+                <!-- <canvas id="myChart" style="width:100%;max-height:400px"></canvas> -->
+                <canvas id="chart"></canvas>
+
             </div>
-            <!-- <div class="cards-1">
-                <div class="card">
-                <div class="card-name">Frequency Sistem Timor</div><hr>
-                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
-                </div>
-            </div>
-            <div class="cards-1">
-                <div class="card">
-                <div class="card-name">Bauran Energi Sistem Timor</div><hr>
-                <div class="card-2">
-                    <div class="content"></div>
-                    <div class="content"></div>
-                </div>
-                </div>
-            </div>
-            <div class="cards-1">
-                <div class="card">
-                <div class="card-name">Biaya Energi Sistem Timor</div><hr>
-                <div class="card-2">
-                    <div class="content"></div>
-                    <div class="content"></div>
-                </div>
-                </div>
-            </div> -->
-            <!-- <div class="cards">
-                <div class="card">
-                <div class="card-name">Heyoo</div>
-                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
-                </div>
-                <div class="card">
-                <div class="card-name">Heyoo</div>
-                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
-                </div>
-            </div> -->
+        </div>
+    </div>
+
+    <div class="main-left">
+        <div class="card-left">
+            <span class="card-name">Langgam Beban Timor</span>
+            <span class="card-value">Langgam Beban Timor</span>
+        </div>
+        <div class="card-left">
+            <span class="card-name">Langgam Beban Timor</span>
+            <span class="card-value">Langgam Beban Timor</span>
+        </div>
+    </div>
 </div>
-
-
-
-
