3bd5904b2353ced2c1944f060c9f97f6fe1a6060
haasstt <nurafiifalmasazhr@gmail.com>
2023-06-03 12:17:36 -0700

    PUSH

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
     <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://d3js.org/d3.v6.min.js"></script>
 
     <!-- Boxiocns CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
@@ -177,8 +178,8 @@
   </div>
 
 </body>
-</html>
+</html>                                       
 
-<script src="assets/js/langgam_beban.js"></script>
-<script src="assets/js/realtime_pembangkit.js"></script>
+<script src="assets/js/realtime.js"></script>
+<script src="assets/js/chart-pie.js"></script>
 <script src="assets/js/index.js"></script>
