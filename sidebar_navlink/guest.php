17bde372f2f1229c2637a4684cc2944fa81e2526
haasstt <nurafiifalmasazhr@gmail.com>
2023-06-05 23:08:35 -0700

    a

--- sidebar_navlink/guest.php
+++ sidebar_navlink/guest.php
@@ -1,7 +1,7 @@
 
 <ul class="nav-links">
       <div class="box-profile">
-        <img src="assets/img/1.png" alt="">
+        <img src="assets/img/foto_profil/default_profil.png" alt="">
         <h3>Guest</h3>
       </div>
       <li>
